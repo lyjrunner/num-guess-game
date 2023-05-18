1b457b4a18a10db3fc94e164535d110acb21caae
youngjune lee <con6329@gmail.com>
2023-05-18 20:36:24 +0900

    second commit

--- main.js
+++ main.js
@@ -7,34 +7,36 @@
 // 5번의 기회를 다쓰면 게임이 끝난다(더이상 추측불가, 버튼이 disable)
 // 유저가 1~100 범위 밖에 숫자를 입력하면 알려준다. 기회를 깎지 않는다
 // 유저가 이미 입력한 숫자를 입력한 숫자를 또입력하면, 알려준다, 기회를 깎지 않는다.
-
-let computernum = 0;
+let comnumber = 0;
 let playbutton = document.getElementById("play-button");
 let userinput = document.getElementById("user-input");
 let resultarea = document.getElementById("result-area");
-playbutton.addEventListener("click",play);
+let resetbutton = document.getElementById("reset-button");
 
-function pickrandomnum(){
-    computernum = Math.floor(Math.random()*100)+1;
-    console.log("정답",computernum);
+playbutton.addEventListener("click", start);
+resetbutton.addEventListener("click", reset);
+
+function randomnumber(){
+    comnumber = Math.floor(Math.random()*(100)+1);
+    console.log("정답",comnumber);
 }
 
-function play(){
-    let userValue = userinput.value;
-    console.log(userValue);
-    if(userValue < computernum){
-        resultarea.textContent ="Down!"
-        console.log("Down!")
+function start(){
+    let uservalue = userinput.value
+    if(uservalue < comnumber){
+        resultarea.textContent="up"
     }
-    else if(userValue > computernum){
-        resultarea.textContent ="Up"
-        console.log("Up")
+    else if(uservalue > comnumber){
+        resultarea.textContent="down"
+    }else
+    {
+        resultarea.textContent="정답입니다^^"
     }
-    else if(userValue = computernum){
-        resultarea.textContent ="정답입니다!!"
-        console.log("정답입니다!!")
-    }
-
+}
+function reset(){
+    userinput.value="";
+    randomnumber()
+    resultarea.textContent="결과값이 여기에 나옵니다"
 }
 
-pickrandomnum();
+randomnumber();
